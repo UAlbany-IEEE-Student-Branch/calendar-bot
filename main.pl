e2b4c442a8f4dd6c182c13623e0cff6c8563ae74
Brian <bstuart777@gmail.com>
2021-08-16 22:16:21 -0400

    updated file extension and added base code for api syncing

--- main.pl
+++
@@ -1,9 +0,0 @@
-import discord 
-from discord.ext import commands
-from discord.ext.commands.bot import Bot
-from dotenv import load_dotenv
-import os
-
-
-load_dotenv('.env')
-TOKEN = os.getenv("IEEE_Bot")
